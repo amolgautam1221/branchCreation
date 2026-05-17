aaee6f2e33c91777c90239486e6716b9c8bcfc33
root <root@ip-172-31-9-162.ap-south-1.compute.internal>
2026-05-17 08:42:22 +0000

    recommit

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+from dotenv import load_dotenv
+import os
+import pymongo
+
+
+load_dotenv()
+
+MONGO_URI = os.getenv('MONGO_URI')
+
+client = pymongo.MongoClient(MONGO_URI)
+
+db = client.test
+
+collection = db['flask-tutorial']
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/submittodoitem", methods=["POST"])
+def submit_todo():
+
+    item_name = request.form.get("itemName")
+    item_description = request.form.get("itemDescription")
+
+    data = {
+        "itemName": item_name,
+        "itemDescription": item_description
+    }
+
+    collection.insert_one(data)
+
+    return "To-Do Item Saved Successfully"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
